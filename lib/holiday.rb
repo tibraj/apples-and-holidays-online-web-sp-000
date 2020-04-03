e1d77c6d6f722c3260c5c0b60ea6945e538dd36c
Taulant Ibraj <tibraj94@gmail.com>
2020-04-03 17:35:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
     if season == :winter
-    :winter.each do |holiday, supply|
+    holiday.each do |holiday, supply|
       :winter[:holiday] << supply
     end
   end
